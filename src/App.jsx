580018c93644751dd13355221fcc61adc1e67f5d
v1ab1 <sharkandmouse@gmail.com>
2023-03-27 20:49:38 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div className="App">
+      <img src="https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg" alt="star wars 1" />
+      <img src="https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg" alt="star wars 2" />
+    </div>
+  );
+}
