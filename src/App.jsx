5d31ae9e39e878148398e2aa439c6d23057c48ae
v1ab1 <sharkandmouse@gmail.com>
2023-03-28 23:03:11 +0500

    menu done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Main } from "./pages/Main";
+import { Menu } from './pages/Menu';
+
 export const App = () => {
+  const arr = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'];
   return (
     <div className="App">
-      <img src="https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg" alt="star wars 1" />
-      <img src="https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg" alt="star wars 2" />
+      <Routes>
+        <Route path='/' element={<Menu />} />
+        {arr.map((el, i) =>
+          <Route path={`/${i+1}`} element={<Main />} />
+        )}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+    return (
+      <div className="kek">
+        23
+      </div>
+    );
+}
