e70f92593d51ef3fe40e6add5cf96cf17e55c927
v1ab1 <sharkandmouse@gmail.com>
2023-04-14 22:39:01 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,9 @@ export const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path='starwars_wiki/' element={<Menu paths={arr} />} />
         <Route path='/' element={<Menu paths={arr} />} />
         {arr.map((el, i) =>
           <Route path={`/${i+1}`} element={<Main chapter={i+1} preview={el} />} key={i} />
-        )}
-        {arr.map((el, i) =>
-          <Route path={`starwars_wiki/${i+1}`} element={<Main chapter={i+1} preview={el} />} key={i} />
         )}
       </Routes>
     </div>
